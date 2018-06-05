bb5b19b4380aa9b56c435db3d9abd9d4b484dd98
grubachybaryba <grubachybaryba@gmail.com>
2018-06-05 18:49:44 +0200

    unicorn.js core functionality

--- unicorn.js
+++ unicorn.js
@@ -1,16 +1,46 @@
+const glossary = {
+    "positive": [
+        "opt out"
+    ],
+    "negative": [
+        "contact",
+        "telephone",
+        "phone",
+        "e-mail",
+        "email",
+        "mail",
+        "marketing"
+    ]
+}
+
 class CheckboxUnicorn {
 
-    constructor() {
+    init() {
+        let checkboxes = document.querySelectorAll("input[type=checkbox]")
+        for (let i = 0; i < checkboxes.length; i++) {
+            const checkboxId = checkboxes[i].getAttribute("id")
+            const checkboxLabel = document.querySelector(`label[for=${checkboxId}]`)
+
+            this.searchInLabel(checkboxLabel)
+        }
     }
 
-    init() {
-        console.log("Checkbox unicorn!")  
+    searchInLabel(label) {
+        const labelText = label.innerText
 
-        var images = document.getElementsByTagName('img')
-        for (var i = 0, l = images.length; i < l; i++) {
-            images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height
+        if (!this.checkGlossary(labelText, "negative")) {
+            label.classList.add("unicorn-negative")
+        } else if (!this.checkGlossary(labelText, "positive")) {
+            label.classList.add("unicorn-positive")
         }
-    }    
+    }
+
+    checkGlossary(text, value) {
+        glossary[value].some(function(item) {
+            return text.indexOf(item) === -1
+        })
+    }  
+    
 }
 
 let unicorn = new CheckboxUnicorn()
